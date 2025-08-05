8d624929be7b314068dea4e5a3d9916a17576844
Isaac-bueno-oliveira <olivr145@gmail.com>
2025-08-05 12:49:53 -0300

    Criando classe de conexão com o servidor local

--- TesteTelaLogin/src/conexaoMySQL/Conexao.java
+++ TesteTelaLogin/src/conexaoMySQL/Conexao.java
@@ -1,5 +1,22 @@
 package conexaoMySQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+	
+	public static Connection conexao_servidor() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			return DriverManager.getConnection("jdbc:mysql://localhost/db_senhas", "root", "@Thor145");
+			
+		} catch (ClassNotFoundException e) {
+			throw new SQLException(e.getException());
+		
+		}
+	}
+	
 	
 }
